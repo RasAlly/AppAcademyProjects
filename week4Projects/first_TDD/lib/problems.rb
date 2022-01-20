41658155608c74f12ae8923bb7e8c2a598db50f5
kingbloopy <kingbloopy@gmail.com>
2022-01-20 14:17:40 -0800

    two_sum

--- week4Projects/first_TDD/lib/problems.rb
+++ week4Projects/first_TDD/lib/problems.rb
@@ -1,3 +1,17 @@
 def remove_dups(arr)
     arr.uniq
 end
+
+class Array
+    def two_sum
+        sum = []
+        (0...self.length-1).each do |i|
+            (i+1...self.length).each do |j|
+                if self[i] + self[j] == 0
+                    sum << [i, j]
+                end
+            end
+        end
+        sum
+    end
+end
--- week4Projects/first_TDD/spec/problems_spec.rb
+++ week4Projects/first_TDD/spec/problems_spec.rb
@@ -7,3 +7,11 @@ describe "remove_dups(arr)" do
         expect(remove_dups([1,2,1,4,4])).to eq([1,2,4])
     end
 end
+
+describe Array do
+    context "Array#two_sum" do
+        it 'finds all pairs of positions where the elements at those positions sum to zero' do
+            expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
+        end
+    end
+end
