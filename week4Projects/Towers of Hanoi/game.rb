0e362d5a1846041a30c38e192c5eac2f2a60de61
kingbloopy <kingbloopy@gmail.com>
2022-01-20 16:11:47 -0800

    error message

--- week4Projects/Towers of Hanoi/game.rb
+++ week4Projects/Towers of Hanoi/game.rb
@@ -1,9 +1,9 @@
 class Game
 
-    attr_accessor :rod1, :rod2, rod3
+    attr_accessor :rod1, :rod2, :rod3
 
     def initialize
-        @rod1 = [1, 2, 3, 4, 5, 6]
+        @rod1 = [1, 2, 3]
         @rod2 = []
         @rod3 = []
     end
@@ -12,11 +12,42 @@ class Game
         
     # end
 
-    def ask_user
+    def which_start
+        current_rod = rod1
         
+        p "select a rod: 1, 2, or 3"
+        input = gets.chomp
+        if input.to_i == 2
+            current_rod = rod2
+        elsif input.to_i == 3
+            current_rod = rod3
+        end
+
+        current_rod
+    end
+
+    def which_end
+        current_rod = rod1
+
+        begin
+            p "select a rod: 1, 2, or 3"
+            input = gets.chomp.to_i
+            if input == 2
+                current_rod = rod2
+            elsif input == 3
+                current_rod = rod3
+            end
+            
+            raise RuntimeError.new("cannot pick same rod, try again") if current_rod == which_start
+        rescue RuntimeError => e
+            puts e.message
+            # puts "try again!"
+            retry
+        end
+        current_rod
     end
 
     def win?
-        (rod3.length) == 6 && (rod3 == rod3.sort)
+        (rod3.length) == 3 && (rod3 == rod3.sort)
     end
 end
