20ba0c79e02e884462fb2dcfc8b660de5a3cade6
Ras Ally <rasally434@gmail.com>
2022-01-20 20:13:12 -0500

    Add play method

--- week4Projects/Towers of Hanoi/game.rb
+++ week4Projects/Towers of Hanoi/game.rb
@@ -1,3 +1,4 @@
+require "byebug"
 class Game
 
     attr_accessor :rod1, :rod2, :rod3
@@ -12,42 +13,71 @@ class Game
         
     # end
 
-    def which_start
-        current_rod = rod1
+    # def add
+    # end
+
+    # def which_start
+    #     current_rod = rod1
         
-        p "select a rod: 1, 2, or 3"
-        input = gets.chomp
-        if input.to_i == 2
-            current_rod = rod2
-        elsif input.to_i == 3
-            current_rod = rod3
+    #     p "select a rod: 1, 2, or 3"
+    #     input = gets.chomp
+    #     if input.to_i == 2
+    #         current_rod = rod2
+    #     elsif input.to_i == 3
+    #         current_rod = rod3
+    #     end
+
+    #     current_rod
+    # end
+
+    def ask_user
+        i = 0
+        while !won? || i == 0
+            puts "#{rod1}, #{rod2}, #{rod3}" if i == 0
+        start_rod = rod1
+        
+        puts "choose a rod to take a disc from: 1, 2, or 3"
+        input = gets.chomp.to_i
+        if input == 2
+            start_rod = rod2
+        elsif input == 3
+            start_rod = rod3
         end
 
-        current_rod
-    end
-
-    def which_end
-        current_rod = rod1
+        removed_disc = start_rod.shift
+        puts "chosen rod: #{start_rod}"
+        puts "chosen disc: #{removed_disc}"
+        ####
+        end_rod = rod1
 
         begin
-            p "select a rod: 1, 2, or 3"
+            puts "select a rod to place disc: 1, 2, or 3"
             input = gets.chomp.to_i
             if input == 2
-                current_rod = rod2
+                end_rod = rod2
             elsif input == 3
-                current_rod = rod3
+                end_rod = rod3
             end
+            raise RuntimeError.new("cannot pick same rod, try again") if end_rod == start_rod
+
+            raise RuntimeError.new("must place disc on top of larger disc") if !end_rod.empty? && removed_disc > end_rod[0]
+            # if end_rod.empty? || removed_disc < end_rod[0]
+                end_rod.unshift(removed_disc)
+            # end 
             
-            raise RuntimeError.new("cannot pick same rod, try again") if current_rod == which_start
         rescue RuntimeError => e
             puts e.message
             # puts "try again!"
             retry
         end
-        current_rod
+        puts "#{rod1}, #{rod2}, #{rod3}"
+        i += 1
+      end
+      return "YOU WIN!!!"
     end
 
-    def win?
-        (rod3.length) == 3 && (rod3 == rod3.sort)
+    def won?
+        rods = [rod1, rod2, rod3]
+        rods.any?{ |rod| (rod.length) == 3 && (rod == rod.sort) }
     end
 end
