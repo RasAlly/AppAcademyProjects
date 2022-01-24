d95d68c0937a5c5d54822fa5ce1d500315e619b9
ajoyvmon <ajoyvmon@gmail.com>
2022-01-24 15:18:03 -0800

    added ResizingIntSet

--- week5Projects/skeleton/lib/p01_int_set.rb
+++ week5Projects/skeleton/lib/p01_int_set.rb
@@ -34,21 +34,25 @@ end
 
 
 class IntSet
+
+  attr_reader :store
+
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
     # p @store
-    @store[num % num_buckets] << num
+    self[num] << num
     # p @store
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
-    @store[num % num_buckets].include?(num)
+    self[num].include?(num)
   end
 
   private
@@ -65,6 +69,7 @@ end
 
 class ResizingIntSet
   attr_reader :count
+  attr_accessor :store
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
@@ -72,17 +77,29 @@ class ResizingIntSet
   end
 
   def insert(num)
+    if !self[num].include?(num)
+      self[num] << num
+      @count += 1
+      resize! if @count > @store.length
+    end
   end
 
   def remove(num)
+    if self[num].include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    # p @store
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
+    @store[num % num_buckets]
     # optional but useful; return the bucket corresponding to `num`
   end
 
@@ -91,5 +108,14 @@ class ResizingIntSet
   end
 
   def resize!
+    array = Array.new(@store.length * 2) { Array.new }
+    @store.each do |sub_arr|
+      sub_arr.each do |num|
+        array[num % array.length] << num
+      end
+    end
+    # p array
+    @store = array
+    # p @store
   end
 end
