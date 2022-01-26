d23d00bd5b2c2fc51a98f44c652f3b903b023119
Ras Ally <rasally434@gmail.com>
2022-01-26 13:39:55 -0500

    Add tables

---
+++ week5Projects/AA Questions/import_db.sql
@@ -0,0 +1,23 @@
+CREATE TABLE users(
+  id INTEGER PRIMARY KEY,
+  fname TEXT NOT NULL,
+  lname TEXT NOT NULL
+);
+
+CREATE TABLE questions(
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  associated_author INTEGER NOT NULL
+  
+  FOREIGN KEY(associated_author) REFERENCES users(id)
+);
+
+CREATE TABLE question_follows(
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL
+
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(question_id) REFERENCES questions(id)
+);
