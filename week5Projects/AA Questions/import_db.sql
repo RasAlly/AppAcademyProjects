81a6fd27d49569aa3fb2865cee8c0ed3fab402d7
Ras Ally <rasally434@gmail.com>
2022-01-26 14:50:31 -0500

    Insert remaining values

--- week5Projects/AA Questions/import_db.sql
+++ week5Projects/AA Questions/import_db.sql
@@ -1,4 +1,9 @@
 PRAGMA foreign_keys = ON;
+DROP TABLE users;
+DROP TABLE questions;
+DROP TABLE question_follows;
+DROP TABLE replies;
+DROP TABLE question_likes;
 
 CREATE TABLE users(
   id INTEGER PRIMARY KEY,
@@ -40,7 +45,7 @@ CREATE TABLE question_likes (
   id INTEGER PRIMARY KEY,
   user_id INTEGER NOT NULL,
   user_question INTEGER NOT NULL,
-  user_like BOOLEAN,
+  user_like INTEGER,
 
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (user_question) REFERENCES questions(id)
@@ -56,18 +61,24 @@ VALUES
 INSERT INTO
   questions (title, body, associated_author)
 VALUES
-  ('Help for problem', 'I''m having trouble with Javascript :(', (SELECT id FROM users WHERE fname = 'John') )
+  ('Help for problem', 'I''m having trouble with Javascript :(', (SELECT id FROM users WHERE fname = 'John') ),
   ('Need help signing up', 'Application page not running', (SELECT id FROM users WHERE fname = 'Sammy') );
 
 INSERT INTO
   question_follows (user_id, question_id)
 VALUES
-  ( (SELECT id FROM users WHERE fname = 'John'), (SELECT id FROM questions WHERE title = 'Help for problem' ) )
+  ( (SELECT id FROM users WHERE fname = 'John'), (SELECT id FROM questions WHERE title = 'Help for problem' ) ),
   ( (SELECT id FROM users WHERE fname = 'Sammy'), (SELECT id FROM questions WHERE title = 'Need help signing up' ) );
 
 INSERT INTO
   replies (question_reply, user_reply, reply_body, parent_id)
 VALUES
-  ( (SELECT id FROM questions WHERE title = 'Help for problem' ), (SELECT id FROM users WHERE fname = 'Sammy'), 'Make sure to use semicolon' ),
-  ( (SELECT id FROM questions WHERE title = 'Need help signing up' ), (SELECT id FROM users WHERE fname = 'John'), 'Please use the link provided in your email' ),
+  ( (SELECT id FROM questions WHERE title = 'Help for problem' ), (SELECT id FROM users WHERE fname = 'Sammy'), 'Make sure to use semicolon', NULL ),
+  ( (SELECT id FROM questions WHERE title = 'Need help signing up' ), (SELECT id FROM users WHERE fname = 'John'), 'Please use the link provided in your email', NULL ),
   ( (SELECT id FROM questions WHERE title = 'Need help signing up' ), (SELECT id FROM users WHERE fname = 'Sammy'), 'There''s no link', (SELECT id FROM replies WHERE reply_body = 'Please use the link provided in your email' ) );
+
+INSERT INTO 
+  question_likes (user_id, user_question, user_like)
+VALUES
+  ( (SELECT id FROM users WHERE fname = 'John'), (SELECT id FROM questions WHERE title = 'Need help signing up'), 1 ),
+  ( (SELECT id FROM users WHERE fname = 'Sammy'), (SELECT id FROM questions WHERE title = 'Help for problem'), 1 );
