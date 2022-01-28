140094cd58e083cebca847fd74a6e344d76d6cfb
Cody Degraffe-Niles <codypdniles@gmail.com>
2022-01-28 18:27:44 -0500

    Finished part 1

--- week5Projects/Movie Buff/skeleton 2/movie_buff/01_queries.rb
+++ week5Projects/Movie Buff/skeleton 2/movie_buff/01_queries.rb
@@ -90,6 +90,14 @@ def directed_by_one_of(them)
   #
   # Find the id and title of all the movies directed by one of 'them'.
 
+  Movie
+    .select(:id, :title)
+    # .select( "movies.id, movies.title")
+    .joins(:director)
+    .where(actors: {name: them} )
+    # .where("actors.name IN (?)", them)
+
+
 end
 
 def movie_names_before_1940
@@ -103,5 +111,9 @@ def movie_names_before_1940
   # improve performace for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
+
+  Movie
+    .where("yr < 1940")
+    .pluck(:title)
 
 end
