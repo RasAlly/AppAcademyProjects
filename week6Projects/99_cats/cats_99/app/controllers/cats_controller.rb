5d9b7ebb1403575b33d5a137da570dc73ffc82eb
Ras Ally <rasally434@gmail.com>
2022-02-04 20:03:36 -0500

    Add edit controller and html file

--- week6Projects/99_cats/cats_99/app/controllers/cats_controller.rb
+++ week6Projects/99_cats/cats_99/app/controllers/cats_controller.rb
@@ -18,4 +18,35 @@ class CatsController < ApplicationController
         @kitty = Cat.new
         render :new
     end
+
+    def create
+        @kitty = Cat.new(cat_params)
+
+        if @kitty.save
+            redirect_to cat_url(@kitty)
+        else
+            render json: @kitty.errors.full_messages, status: 422
+        end
+    end
+
+    def edit
+        @kitty = Cat.find(params[:id])
+        render :edit
+    end
+
+    def update
+        @kitty = Cat.find(params[:id])
+
+        if @kitty.update(cat_params)
+            redirect_to cat_url(@kitty)
+        else
+            render json: @kitty.errors.full_messages, status: 422
+        end
+    end
+
+    private
+
+    def cat_params
+        params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
+    end
 end
