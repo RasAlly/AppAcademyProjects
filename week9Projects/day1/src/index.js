8286904ea858fee7f8bf74822ed4ebcc68214156
Ras Ally <rasally434@gmail.com>
2022-02-21 14:07:32 -0500

    stuff

--- week9Projects/day1/src/index.js
+++ week9Projects/day1/src/index.js
@@ -1,3 +1,4 @@
+console.log("hello");
 const MovingObject = require("./moving_object.js");
 
 window.MovingObject = MovingObject;
--- week9Projects/day1/src/moving_object.js
+++ week9Projects/day1/src/moving_object.js
@@ -5,4 +5,20 @@ function MovingObject(obj) {
   this.color = obj['color'];
 }
 
+MovingObect.prototype.draw = function(ctx) {
+  document.addEventListener("DOMContentLoaded", function(){
+    const canvasEl = document.getElementById("game-canvas");
+  
+    const ctx = canvasEl.getContext("2d");
+  
+    ctx.beginPath();
+    ctx.arc(100, 100, 90, 0, 2*Math.PI, true);
+    ctx.strokeStyle = "green";
+    ctx.lineWidth = 5;
+    ctx.stroke();
+    ctx.fillStyle = "blue";
+    ctx.fill();
+  });
+}
+
 module.exports = MovingObject;
