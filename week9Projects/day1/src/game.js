6f48ce1a3675109eba4e35e122f5b3e77c4c52cd
Ras Ally <rasally434@gmail.com>
2022-02-21 19:17:24 -0500

    Add stuff

---
+++ week9Projects/day1/src/game.js
@@ -0,0 +1,15 @@
+function Game(dim_x, dim_y, num_asteroids) {
+  this.dim_x = dim_x;
+  this.dim_y = dim_y;
+  this.num_asteroids = num_asteroids;
+  this.addAsteroids();
+}
+
+
+Game.prototype.addAsteroids = function () {
+  
+}
+
+Game.prototype.randomPosition = function () {
+  let pos = []
+}
