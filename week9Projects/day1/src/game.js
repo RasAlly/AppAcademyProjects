3f201f73cff936798e61fccaa9cd0b3236e33565
Ras Ally <rasally434@gmail.com>
2022-02-21 20:07:05 -0500

    Add stuff again

--- week9Projects/day1/src/game.js
+++ week9Projects/day1/src/game.js
@@ -1,15 +1,41 @@
-function Game(dim_x, dim_y, num_asteroids) {
-  this.dim_x = dim_x;
-  this.dim_y = dim_y;
-  this.num_asteroids = num_asteroids;
+const Asteroid = require("./asteroid");
+
+const DIM_X = 500;
+const DIM_Y = 500;
+const NUM_ASTEROIDS = 20;
+
+function Game() {
   this.addAsteroids();
+  this.asteroids = []
 }
 
 
 Game.prototype.addAsteroids = function () {
-  
+  for (let i = 0; i <= 20; i++) {
+    let a = new Asteroid(this.randomPosition())
+    this.asteroids.push(a);
+  }
+}
+
+Game.prototype.draw = function(ctx) {
+  ctx.clearRect(0, 0, DIM_X, DIM_Y);
+  this.asteroids.forEach(a => {
+    a.draw(ctx);
+  })
+}
+
+Game.prototype.moveObjects = function () {
+  this.asteroids.forEach(a => {
+    
+  })
 }
 
 Game.prototype.randomPosition = function () {
-  let pos = []
+  let arr = []
+  let min = 0;
+  let max = 500;
+  arr.push(Math.floor(Math.random() * (max - min + 1)) + min);
+  arr.push(Math.floor(Math.random() * (max - min + 1)) + min);
+  return arr;
 }
+
