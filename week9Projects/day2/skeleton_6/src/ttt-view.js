5f68135ce4c0f763637202097367df81a622db02
JonJWong <wongjonathan01@gmail.com>
2022-02-22 20:05:29 -0500

    finish

--- week9Projects/day2/skeleton_6/src/ttt-view.js
+++ week9Projects/day2/skeleton_6/src/ttt-view.js
@@ -1,12 +1,16 @@
+const Game = require("../ttt_node/game");
+
 class View {
   constructor(game, el) {
     this.game = game;
     this.grid = el;
+    this.handleClick = this.handleClick.bind(this);
     this.setupBoard();
+    this.bindEvents();
   }
 
   setupBoard() {  
-    const list = document.createElement("ul")
+    const list = document.createElement("ul");
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         let square = document.createElement("li");
@@ -23,12 +27,12 @@ class View {
 
   handleClick(e) {
     const grid = e.target
-    grid.nodeName === "LI"
+    "LI" === grid.nodeName && this.makeMove(grid);
   }
 
   makeMove(square) {
-    const location = JSON.parse(grid.dataset.location)
-    const player = this.game.currentPlayer
+    const location = JSON.parse(square.dataset.location);
+    const player = this.game.currentPlayer;
     try {
       this.game.playMove(location)
     }
@@ -36,6 +40,18 @@ class View {
       alert(square.msg)
     }
     square.classList.add(player);
+    if (this.game.isOver()) {
+      if (this.game.winner() === null) {
+        alert(`It's a tie!`)
+      } else {
+        const marks = document.querySelectorAll(`.${player}`)
+        marks.forEach(ele => {
+          ele.classList.add(player+"-winner")
+        })
+        alert(`${player} has won!`);
+      }
+      this.grid.removeEventListener("click", this.handleClick)
+    };
   }
 
 }
