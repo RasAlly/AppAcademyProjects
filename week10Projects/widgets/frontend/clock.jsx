f32824eb2d382dde60c6cb123b2aea2b21def834
Ras Ally <rasally434@gmail.com>
2022-03-04 18:54:41 -0500

    Add clock widget

---
+++ week10Projects/widgets/frontend/clock.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {time: new Date()}
+    this.tick = this.tick.bind(this)
+  }
+
+  tick() {
+    const time = new Date();
+    this.setState({ time });
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(this.tick, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  render() {
+    let hours = this.state.time.getHours();
+    let mins = this.state.time.getMinutes();
+    let secs = this.state.time.getSeconds();
+
+    return(
+      <div>
+        <h1>Clock</h1>
+        <p className="timer">{hours}::{mins}::{secs}</p>
+      </div>
+    )
+  }
+}
+
+export default Clock;
