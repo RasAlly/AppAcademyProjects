13e4821f2c0f301ffad3ff441768461d5035fdbd
Catherine Choi <catherinemargaretchoi@gmail.com>
2022-03-08 19:11:10 -0500

    Add root reducer

--- week11Projects/todos/frontend/reducers/todos_reducer.js
+++ week11Projects/todos/frontend/reducers/todos_reducer.js
@@ -1,10 +1,49 @@
 import {receiveTodos, receiveTodo} from '../actions/todo_actions';
+import { RECEIVE_TODO, RECEIVE_TODOS } from '../actions/todo_actions';
 
-const todosReducer = (state = {}, action) => {
+const initialState = {
+  1: {
+    id: 1,
+    title: "wash car",
+    body: "with soap",
+    done: false
+  },
+  2: {
+    id: 2,
+    title: "wash dog",
+    body: "with shampoo",
+    done: true
+  }
+};
+
+// [{
+//   id: 1,
+//   title: "wash car",
+//   body: "with soap",
+//   done: false
+// },
+// {
+//   id: 2,
+//   title: "wash dog",
+//   body: "with shampoo",
+//   done: true
+// }]
+
+const todosReducer = (state = initialState, action) => {
   Object.freeze(state);
   const nextState = Object.assign({}, state);
 
   switch (action.type) {
+    case RECEIVE_TODOS:
+      const obj = {}
+      action.todos.forEach((todo)=> {
+        // nextState[todo.id] = todo // This is for adding to existing state, not replacing
+        obj[todo.id] = todo
+      });
+      return obj;
+    case RECEIVE_TODO:
+      nextState[action.todos.id] = action.todos
+      return nextState;
     default:
       return state;
   }
--- week11Projects/todos/frontend/todo_redux.jsx
+++ week11Projects/todos/frontend/todo_redux.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from '../frontend/store/store';
+// import rootReduct from '../frontend/reducers/root_reducer'
+import {receiveTodos,receiveTodo } from '../frontend/actions/todo_actions'
 
 document.addEventListener("DOMContentLoaded", () => {
   const content = document.getElementById('content')
 
   ReactDOM.render(<h1>Test</h1> ,content)
 
-  const store = configureStore;
-  window.store = store
+  const store = configureStore();
+  window.store = store;
+  window.receiveTodos = receiveTodos;
+  window.receiveTodo = receiveTodo;
+
 })
